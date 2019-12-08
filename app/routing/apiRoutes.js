ba182887c6d19ebccb6b0ffe485b2e01a9b45a73
Robert Moore <rcmoore3@ncsu.edu>
2019-12-07 23:34:00 -0500

    finished api routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,16 +14,34 @@ module.exports = function(app) {
 
       var newUserScore = req.body.scores
       var scoreArray = [];
+      var closestMatch = 0;
     
-      // declare vars
-      // how to let js know a var should be a number
+      // first loop
 
+      for(var i = 0; i<importedFriendsObject.length;i++){
+        var scoresDifference = 0;
+         //run through scores to compare friends
+      for(var j=0; j<newUserScore.length; j++){
+        scoresDifference += (Math.abs(parseInt(importedFriendsObject[i].scores[j]) - parseInt(newUserScore[j])));
+      }
+      scoreArray.push(scoresDifference);
+    }
+    for(var i=0; i<scoreArray.length; i++){
+      if(scoreArray[i] <= scoreArray[closestMatch]){
+        closestMatch = i;
+      }
+    }
 
-      });
-    };
-   
+    var closestMatch = importedFriendsObject[closestMatch];
+    res.json(closestMatch);
 
-
+    //pushes new submission into the friends object array
+    importedFriendsObject.push(req.body);
+  });
+      };
+    
+  // declare vars
+  // how to let js know a var should be a number
 
 
     // cycle through scores and determine best match
